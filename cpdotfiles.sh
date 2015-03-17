08e671296a9edefb51498c6cbb5daca108507882
gisphm <phmfk@hotmail.com>
2015-03-17 19:50:02 +0800

    changed something
    Signed-off-by: gisphm <phmfk@hotmail.com>

--- cpdotfiles.sh
+++
@@ -1,8 +0,0 @@
-cp ~/.gitconfig gitconfig
-cp ~/.vimrc.before.local vimrc.before.local
-cp ~/.vimrc.local vimrc.local
-cp ~/.vimrc.bundles.local vimrc.bundles.local
-cp ~/.zshenv zshenv
-cp ~/.zshrc zshrc
-cp ~/.oh-my-zsh/custom/themes/phm.zsh-theme phm.zsh-theme
-cp ~/.gemrc gemrc
